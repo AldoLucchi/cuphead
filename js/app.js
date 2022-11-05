d722d3accac5532831337e1c2d41833f610a2cd2
Aldo Lucchi <pokeraldolucchi@hotmail.com>
2022-11-05 06:24:20 -0300

    Se agrega el proyecto

---
+++ js/app.js
@@ -0,0 +1,6 @@
+// Variables
+const enemigosNivelOne = document.querySelector('.enemies1');
+const enemigosNivelTwo = document.querySelector('.enemies2');
+const enemigosNivelThree = document.querySelector('.enemies3');
+const enemigosNivelFour = document.querySelector('.enemies4');
+
